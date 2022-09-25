27e719b83260fcc6f2d539adab54b711a68f4103
CCnova <caiocnova1@gmail.com>
2022-09-25 19:21:05 -0300

    Makes use of prisma querying all users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Listen to port ${PORT}`));
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch((e) => {
+    console.error(e);
+
+    return prisma.$disconnect();
+  });
+
+// app.listen(PORT, () => console.log(`Listen to port ${PORT}`));
